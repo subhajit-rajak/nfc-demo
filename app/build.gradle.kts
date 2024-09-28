d0c0857eb928e25c2535086e5804091218e59515
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-09-29 00:38:37 +0530

    pending orders list from firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -50,4 +51,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation (libs.lottie)
+    implementation(libs.firebase.database)
 }
